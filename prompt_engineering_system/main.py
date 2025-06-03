aa05271ef78208d036c112553cf0602845f491bb
shravya312 <shravyahjainjain@gmail.com>
2025-06-03 13:18:27 +0530

    Added prompt_engineering_system/main.py

--- prompt_engineering_system/main.py
+++ prompt_engineering_system/main.py
@@ -10,13 +10,14 @@ class PromptEngineeringSystem:
         self.applications = SpecializedApplications()
 
     def run_application(self, task_type, **kwargs):
+        reasoning_type = kwargs.pop("reasoning_type", "standard")
         if task_type == "summarization":
-            return self.applications.summarize_text(kwargs["text"])
+            return self.applications.summarize_text(kwargs["text"], reasoning_type=reasoning_type)
         elif task_type == "code_generation":
-            return self.applications.generate_code(kwargs["task_description"], kwargs["language"])
+            return self.applications.generate_code(kwargs["task_description"], kwargs["language"], reasoning_type=reasoning_type)
         elif task_type == "data_extraction":
-            return self.applications.extract_data(kwargs["text"], kwargs["fields"])
+            return self.applications.extract_data(kwargs["text"], kwargs["fields"], reasoning_type=reasoning_type)
         elif task_type == "question_answering":
-            return self.applications.answer_question(kwargs["context"], kwargs["question"])
+            return self.applications.answer_question(kwargs["context"], kwargs["question"], reasoning_type=reasoning_type)
         else:
             raise ValueError(f"Unknown task_type: {task_type}")
